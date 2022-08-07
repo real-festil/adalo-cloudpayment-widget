ab05f5ab820bccf111f6166e956771af1413431f
Roman Gladyshev <romangladyshev@MacBook-Pro-Roman.local>
2022-08-07 14:22:31 +0400

    init commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: `*`,
+        destination: `*`,
+      },
+    ];
+  },
+};
